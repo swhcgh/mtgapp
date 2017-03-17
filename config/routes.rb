4009f08eeb0085b5522edefec9ebc78b9bef471c
Joseph Owens <joseph.t.owens@wmich.edu>
2017-03-17 17:35:05 -0400

    broke routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :cards
   resources :tournaments
   resources :users
-  get 'tournaments#begintournament'
+  get 'tournaments/begintournament' => 'tournaments#begintournament'
   root 'tournaments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
